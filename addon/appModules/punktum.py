dfb9633e10fac9d48c8b9c4e4102ae310a695b02
Joseph Lee <joseph.lee22590@gmail.com>
2026-06-30 09:53:28 -0600

    App modules: add Punktum ap module (initialy a global plugin provided by the program developer). Re #2

---
+++ addon/appModules/punktum.py
@@ -0,0 +1,27 @@
+# Unterstützung für "Punktum"
+import api
+import braille
+import globalCommands
+import globalPluginHandler
+import threading
+
+def setCursorAndClick(routingIndex):
+  braille.handler.routeTo(routingIndex)
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+  def script_braille_routeTo(self, gesture):
+    routingIndex = gesture.routingIndex
+    region = braille.handler.mainBuffer
+    cursorIndex = region.cursorPos
+    braille.handler.routeTo(routingIndex)
+    if cursorIndex != routingIndex:
+      fg = api.getForegroundObject()
+      FormClassName = fg.windowClassName
+      obj = api.getFocusObject()
+      MemoClassName = obj.windowClassName
+      if FormClassName == "TAT_Text_Dlg" and MemoClassName == "TMemo":
+        Timer = threading.Timer(0.050, setCursorAndClick, args=(routingIndex, ))
+        Timer.start()
+
+globalCommands.GlobalCommands.script_braille_routeTo = GlobalPlugin().script_braille_routeTo
